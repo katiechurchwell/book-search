63cec56859d9b39db2bb250586715ba89f845d83
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-04-08 11:29:09 -0700

    formatting

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search', {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search', 
+{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
